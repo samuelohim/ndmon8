9ccd73fcc88e3929ce572fef37e5a1fc54248fa5
Samuel Y <samuel.y@weimeit.com>
2017-06-05 17:51:51 +0800

    ejs layout implemented, home page created

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const eo = require('express');
 app = eo();
+const el = require('express-ejs-layouts');
+
+app.use(el);
+
+app.use(eo.static(__dirname+'/public/css'));
+app.set('view engine', 'ejs');
+
 port = process.env.PORT || 9000;
 
 router = require('./app/router.js');
